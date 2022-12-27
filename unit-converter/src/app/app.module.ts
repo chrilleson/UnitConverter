5d72491335c683b57a0f616d4efeedbd3a97f6be
Christoffer Ljungqvist <Christoffer.Ljungqvist@hotmail.se>
2022-12-27 19:30:51 +0100

    Added Ngxs

--- unit-converter/src/app/app.module.ts
+++ unit-converter/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule
+    NgxsModule.forRoot([UiState],  { developmentMode: environment.production }),
+    NgxsReduxDevtoolsPluginModule.forRoot({ disabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
