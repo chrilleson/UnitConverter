e7b091731b980146efed53c10679b0451633d0cd
Christoffer Ljungqvist <Christoffer.Ljungqvist@hotmail.se>
2023-03-30 22:51:30 +0200

    Implemented enheter component

--- unit-converter-web/src/components/main.tsx
+++ unit-converter-web/src/components/main.tsx
@@ -1,12 +1,18 @@
-import { Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Toolbar } from "@mui/material";
+
+import { Enheter } from "./enheter/enheter";
 
 export const Main = () => {
   return(
-    <main>
-      <Container>
-      <Grid container> 
-      </Grid>
-    </Container>
-    </main>    
+    <Box component='main'>
+      <Toolbar />
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={2}>
+          <Grid item xs>
+            <Enheter />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>    
   );
 }
