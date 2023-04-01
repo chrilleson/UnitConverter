f87be5570e6a4f8d3ac41d5d3d90d21215bef246
Christoffer Ljungqvist <Christoffer.Ljungqvist@hotmail.se>
2023-04-01 11:13:42 +0200

    Added some models

--- unit-converter-web/src/models/unit-types.ts
+++ unit-converter-web/src/models/unit-types.ts
@@ -1,3 +1,13 @@
 export interface UnitType {
   Name: string;
 }
+
+export interface UnitModel {
+  convertFrom: Unit;
+  convertTo: Unit;
+}
+
+export interface Unit {
+  value: Number;
+  unitType: string;  
+}
