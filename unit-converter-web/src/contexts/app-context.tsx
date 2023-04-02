d84b1674cfe3dd61c197d9b441567beb617f461b
Christoffer Ljungqvist <Christoffer.Ljungqvist@hotmail.se>
2023-04-02 14:09:42 +0200

    Moved theme to ui-context

--- unit-converter-web/src/contexts/app-context.tsx
+++ unit-converter-web/src/contexts/app-context.tsx
@@ -2,26 +2,40 @@ import { createContext, useContext, useReducer } from "react";
 
 import { UnitModel } from "../models/unit-types";
 
-export const AppContext = createContext<any>({});
+export const AppContext = createContext<any>(undefined!);
+export const AppContextDispatch = createContext<any>(undefined!);
 
 export const AppContextProvider = ({children}:any) => {
-  const [state, dispatch] = useReducer(appReducer, initialState);
+  const [state, dispatch] = useReducer(appReducer, initialState);  
   
-  return <AppContext.Provider value={{state, dispatch}}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={state}>
+      <AppContextDispatch.Provider value={dispatch}>{children}</AppContextDispatch.Provider>
+    </AppContext.Provider>
+  );
 }
 
-export const useAppContext = () => useContext(AppContext);
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("useAppContext must be used within a AppContextProvider");    
+  }
+  return context;
+};
+export const useAppContextDispatch = () => useContext(AppContextDispatch);
 
 const initialState = {
   error: { openDialog: false, message: '' },
+  theme: 'light',
   units: [] as UnitModel[]
 }
 
 const appReducer = (state: any, action: any) =>{
-  const { error, units } = state;
+  const { error, theme, units } = state;
 
   return {
     error: errorReducer(error, action),
+    theme: themeReducer(theme, action),
     units: unitsReducer(units, action)
   }
 }
@@ -44,6 +58,18 @@ const errorReducer = (state: any, action: any) => {
       return state;
   }
 };
+
+const themeReducer = (state: any, action: any) => {
+  switch(action.type){
+    case 'THEME_LIGHT': {
+      return 'light';
+    }
+    case 'THEME_DARK': {
+      return 'dark';
+    }
+    default: return state;
+  }
+}
 
 const unitsReducer = (state: any, action: any) => {
   switch(action.type) {
