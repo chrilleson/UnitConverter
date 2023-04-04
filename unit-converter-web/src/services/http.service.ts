9bba34e194b4358776482dc710e41e3fccee99f4
Christoffer Ljungqvist <Christoffer.Ljungqvist@hotmail.se>
2023-04-04 22:12:01 +0200

    Fixed axios setup

--- unit-converter-web/src/services/http.service.ts
+++ unit-converter-web/src/services/http.service.ts
@@ -1,7 +1,32 @@
-import axios from 'axios';
+import axios, { AxiosHeaders } from 'axios';
 
-export const setupAxios = (state: any, action: any) => {
+export const setupAxios = (axios: any, store: any) => {
   axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+  axios.interceptors.request.use(
+    (request: { headers: AxiosHeaders }) => {
+      request.headers.set('Content-type', 'application/json');
+
+      return request;
+    },
+    (error: any) => {
+      return Promise.reject(error);
+    }
+  )
+
+  axios.interceptors.response.use(
+    (response: any) =>{
+      return response;
+    },
+    (error: any) => {
+      console.warn(error);
+      
+      const message = error.response.data.message || error.message;
+
+      store.dispatch({ type: 'SHOW_ERROR', error: { openDialog: true, message: message }});
+      return Promise.reject(error);
+    }
+  )
 }
 
 export default axios;
